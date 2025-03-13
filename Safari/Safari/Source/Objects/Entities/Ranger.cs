2d29b40eed2d9d5366c69f16e3cf99462a6f596a
Zelenák András <zelenakandras2003@gmail.com>
2025-03-13 20:24:11 +0100

    Add skeleton for poacher and ranger entities

---
+++ Safari/Safari/Source/Objects/Entities/Ranger.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Safari.Objects.Entities.Animals;
+
+namespace Safari.Objects.Entities;
+public class Ranger : Entity {
+	public const int SALARY = 1500;
+
+	private static AnimalSpecies? defaultTarget;
+	private AnimalSpecies? targetSpecies;
+	
+	public static AnimalSpecies? DefaultTarget {
+		get => defaultTarget;
+		set => defaultTarget = value;
+	}
+	public AnimalSpecies? TargetSpecies {
+		get => targetSpecies;
+		set => targetSpecies = value;
+	}
+
+	public Ranger(Vector2 pos) : base(pos) {
+		displayName = "Ranger";
+	}
+}
