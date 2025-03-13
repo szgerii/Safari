cbd6d81d847efec0de6e393503c8420d4bfc9966
Zelenák András <zelenakandras2003@gmail.com>
2025-03-13 20:17:17 +0100

    Add the animal subclasses
    - elephant, zebra, lion, tiger

---
+++ Safari/Safari/Source/Objects/Entities/Animals/Zebra.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Safari.Objects.Entities.Animals;
+
+public class Zebra : Animal {
+	public Zebra(Vector2 pos) : base(pos) {
+		displayName = "Zebra";
+		species = AnimalSpecies.Zebra;
+	}
+}
